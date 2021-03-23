ae0b70875c71a9089ff190ecb4f2ac3d3c15042b
Rizqidoet Albetawi <muhamadrizqifadila@gmail.com>
2021-03-23 16:01:59 +0700

    update landing page transition

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -4,12 +4,15 @@ const VhamburgerId = document.getElementById('menu-hamburger');
 VhamburgerId.addEventListener('click', function () {
 	const vmenuId = document.getElementById('menu');
 	if (vmenuId.className.indexOf('opacity-0') > -1) {
-		addClass(VhamburgerId, 'fixed top-0 right-0 text-black mt-2 mr-2');
+		addClass(VhamburgerId, 'fixed top-0 right-0 text-black');
 		removeClass(VhamburgerId, 'relative');
-		addClass(vmenuId, 'bg-gray-100 opacity-100 p-5 top-0 right-0');
+		addClass(
+			vmenuId,
+			'bg-gray-100 opacity-100 p-5 top-0 right-0 transition duration-400',
+		);
 		removeClass(vmenuId, 'opacity-0 invisible');
 	} else {
-		removeClass(VhamburgerId, 'fixed top-0 right-0 text-black mt-2 mr-2');
+		removeClass(VhamburgerId, 'fixed top-0 right-0 text-black');
 		addClass(VhamburgerId, 'relative');
 		removeClass(vmenuId, 'bg-gray-100 opacity-100 p-5 top-0 right-0');
 		addClass(vmenuId, 'opacity-0 invisible');
